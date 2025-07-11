5d74d066f27b83d2a845efe405cafd629b7a2e54
donald-duck122 <19033@student.macleans.school.nz>
2025-07-11 23:11:10 +1200

    added cheeky edit

--- first_bot/bot.py
+++ first_bot/bot.py
@@ -84,6 +84,7 @@ def handle_place_tile(query: QueryPlaceTile, game: Game) -> MovePlaceTile:
     # for tileIndex in validPlacements:
     #     coords = validPlacements[tileIndex][0]
 
+    #haha cheeky edit
     # Logic to determine highest scoring placement
 
     # tileToPlace = hand[maxScoringIndex]
