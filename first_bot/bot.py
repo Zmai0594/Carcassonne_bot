7850d445fa8f4f70e9e41a3fd121a7b98452a630
Zhikai Mai <zmai0594@uni.sydney.edu.au>
2025-07-11 10:38:19 +1000

    initialise valid placements when game starts

--- first_bot/bot.py
+++ first_bot/bot.py
@@ -9,18 +9,44 @@ from lib.interface.queries.query_place_tile import QueryPlaceTile
 from lib.interface.queries.query_place_meeple import QueryPlaceMeeple
 from lib.interface.events.moves.typing import MoveType
 from lib.models.tile_model import TileModel
-
-
-class BotState:
-    def __init__(self):
-        self.last_tile: TileModel | None = None
+from src.helper.client_state import ClientSate
 
 
 def main():
     game = Game()
-    bot_state = BotState()
+    botState = ClientSate()
+    
+    validPlacements = {}
+    cards = botState.my_tiles
+    grid = game.state.map._grid
+    height = len(grid)
+    width = len(grid[0]) if height > 0 else 0
 
+    for card in cards:
+        # (if card in validPlacements check) is outside the O(n^2) loop to save computation time
+        if card in validPlacements:
+            for x in range(height):
+                for y in range(width):
+                    if game.can_place_tile_at(card, x, y):
+                        validPlacements[card].append((x, y))
+        else:
+            for x in range(height):
+                for y in range(width):
+                    if game.can_place_tile_at(card, x, y):
+                        validPlacements[card] = [(x, y)]
 
+    while True:
+        query = game.get_next_query()
+
+        def choose_move(query: QueryType) -> MoveType:
+            match query:
+                case QueryPlaceTile() as q:
+                    return handle_place_tile(game, botState, q)
+
+                case QueryPlaceMeeple() as q:
+                    return handle_place_meeple(game, botState, q)
+
+        game.send_move(choose_move(query))
 
 if __name__ == "__main__":
     main()
