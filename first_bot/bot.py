72aed15f0dce1df94d8c281ba875a37ab7d8f593
Zhikai Mai <zmai0594@uni.sydney.edu.au>
2025-07-14 18:38:24 +1000

    Fixed module errors

--- first_bot/bot.py
+++ first_bot/bot.py
@@ -2,7 +2,7 @@ from lib.interface.events.moves.move_place_meeple import (
     MovePlaceMeeple,
     MovePlaceMeeplePass,
 )
-from src.helper.helper.game import Game
+from helper.game import Game
 from lib.interface.events.moves.move_place_tile import MovePlaceTile
 from lib.interface.queries.typing import QueryType
 from lib.interface.queries.query_place_tile import QueryPlaceTile
@@ -11,9 +11,9 @@ from lib.interface.events.moves.typing import MoveType
 from lib.models.tile_model import TileModel
 from lib.interact.tile import Tile
 from lib.interact.tile import TileModifier
-from src.helper.helper.client_state import ClientSate
+from helper.client_state import ClientSate
 from collections import deque
-from src.lib.lib.interact.structure import StructureType
+from lib.interact.structure import StructureType
 from lib.interact.tile import TileModifier
 from enum import Enum, auto
 from lib.config.map_config import MAX_MAP_LENGTH, MAP_CENTER
